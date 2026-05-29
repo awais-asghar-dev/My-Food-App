91f7a05ec276b0911248defe79bfc201f093c5f4
Muhammad Owais <aa4489083@gmail.com>
2026-05-29 11:41:56 -0700

    Manual deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1152545487"
+    serviceWorkerVersion: "764342223"
   }
 });
